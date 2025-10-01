9521fdc8a6d38a85d0d422ec4c7d28314e6823ac
moormaninmotion-byte <moormaninmotion@gmail.com>
2025-10-01 18:21:29 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-        base: 'SHAP-House-Price-Model',
+        base: '/SHAP-House-Price-Model/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
